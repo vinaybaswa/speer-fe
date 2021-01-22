15e86e927912e17bcd79c33b978f890988fd1522
vinay baswa <vinaybaswa@gmail.com>
2021-01-22 00:48:44 -0500

    basic carousel works

---
+++ src/components/CarouselData.js
@@ -0,0 +1,14 @@
+export const CarouselData = [
+  {
+    image:
+      'https://public-v2links.adobecc.com/2fc0268c-c59f-4ac5-46f8-fe3c819f1e6f/component?params=component_id%3Ac2ea7f76-0124-4d10-bc03-bc2be67c5e12&params=version%3A0&token=1611337529_da39a3ee_9315e3b6310dcdfb9f2f19526d38b01a60fa9a01&api_key=CometServer1'
+  },
+  {
+    image:
+      'https://public-v2links.adobecc.com/2fc0268c-c59f-4ac5-46f8-fe3c819f1e6f/component?params=component_id%3A046e33d2-3f2f-4a90-9b25-f80265473230&params=version%3A0&token=1611337529_da39a3ee_9315e3b6310dcdfb9f2f19526d38b01a60fa9a01&api_key=CometServer1'
+  },
+  {
+    image:
+      'https://public-v2links.adobecc.com/2fc0268c-c59f-4ac5-46f8-fe3c819f1e6f/component?params=component_id%3A8dcf47fb-7b2e-465b-ac6e-266f0d72c0cd&params=version%3A0&token=1611337529_da39a3ee_9315e3b6310dcdfb9f2f19526d38b01a60fa9a01&api_key=CometServer1'
+  }
+];
